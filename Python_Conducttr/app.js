b43d48b5ea9b0cbcac67e87fc0c3d3269c3a4b95
dmaheswaran1998 <dhanyamahe@hotmail.com>
2020-11-02 21:37:40 +1100

    edited JS

--- Python_Conducttr/app.js
+++ Python_Conducttr/app.js
@@ -1,19 +1,38 @@
 function initial() {
   
-  var selector=d3.select("#selDataset");
 
   //updating the dropdown menu
 
     d3.json("player_data.json").then(function(data) {
         interaction=data[0].impersonations;
-        for (const [key] of Object.entries(interaction)) {
-            selector
-            .append("option")
-            .append("value")
-            .text(`${key}`)
-            .property("value", name)
+        // for (const [key] of Object.entries(interaction)) {
+        //     selector
+        //     .append("option")
+        //     .append("value")
+        //     .text(`${key}`)
+        //     .property("value", name)
             
+        // }
+
+        var2=[]
+        for (const [key] of Object.entries(interaction)) {
+            var2.push(key);
+            
+        
         }
+
+        console.log(var2);
+
+
+
+        var time_data=data[3].time_list;
+
+
+
+
+        autocomplete(document.getElementById("myInput"), time_data);
+        autocomplete(document.getElementById("myInput2"), time_data);
+        autocomplete(document.getElementById("myInput3"), var2);
 
        
 
@@ -27,23 +46,26 @@ function initial() {
         // updatePanel(initial_top_cont)
     }); 
 
-   
-   
+
   
   
 }
 
-d3.select("#selDataset").on("change", optionChanged);
+d3.select("#submit_player").on("click", optionChanged);
   
 function optionChanged() {
-    var selector=d3.select("#selDataset");
   //Assign the value of the dropdown menu option to a variable
-    var dropdown_value = selector.property("value");
+
+
+    var dropdown_value = d3.select("#myInput3").node().value;
+
+
 
     d3.json("player_data.json").then(function(data) {
         person_data=data[0].impersonations;
         from_data=data[1].from;
         to_data=data[2].to;
+        time_data=data[4];
         // reply_frequency=data[2]
         // reply_total=data[0]
         // reply_time=data[3]
@@ -84,8 +106,12 @@ function optionChanged() {
         
         }
 
+
+
     
-    });   
+    });  
+    
+
 
 
 
@@ -103,6 +129,14 @@ function optionChanged() {
 
 
 }
+
+d3.select("#clear_player_data").on("click", clearPlayer);
+
+function clearPlayer () {
+    d3.select("#myInput3").node().value = "";
+}
+
+
 
 //         
 
@@ -332,6 +366,218 @@ function updateBar2(newdata) {
 
 }
 
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+
+
+function handleSubmit() {
+    // Prevent the page from refreshing
+  
+    // Select the input value from the form
+    var dropdown_value = d3.select("#myInput3").node().value;
+
+    var start1= d3.select("#myInput").node().value;
+    //console.log(start_date);
+
+    var end1 = d3.select("#myInput2").node().value;
+
+
+    d3.json("player_data.json").then(function(data) {
+
+
+        
+        var time_data=data[3].time_list;
+
+        //var final_date_length=time_data.length;
+        var final_date_length=time_data.length-1;
+        var final_date=time_data[`${final_date_length}`]
+
+        if (start1==="") {
+            start1=time_data[0];
+        }
+
+        console.log(start1);
+
+        if (end1==="") {
+            end1=time_data[`${final_date_length}`]
+        }
+
+        console.log(end1);
+
+
+
+        //console.log(end_date);
+
+        time_data=data[4];
+
+        time_list1=data[3].time_list
+
+        for (var i = 0; i < time_list1.length; i++) {
+            if (time_list1[i]===start1){
+                var start_index=i;
+                console.log(start_index);
+            }
+
+            if (time_list1[i]===end1){
+                var end_index=i;
+                console.log(end_index);
+            }
+                
+            //Do something
+        }
+
+        
+
+
+        for (const [key, value] of Object.entries(time_data)) {
+            if (key==dropdown_value){
+                
+                var interaction=value;
+                console.log(interaction);
+
+            }  
+           
+            
+        
+        }
+
+    
+        
+        
+        
+    });   
+    
+    d3.select("#myInput").node().value = "";
+    d3.select("#myInput2").node().value = "";
+
+
+
+
+
+
+
+
+       
+
+    // }); 
+  
+    //console.log(end_date);
+  
+    // clear the input value
+
+
+    console.log();
+  
+    // Build the plot with the new stock
+    // buildPlot(stock);
+}
+  
+// function buildPlot(stock) {
+
+//     var stock = 3;
+//     console.log(stock);
+  
+// }
+ 
+// Add event listener for submit button
+d3.select("#submit_dates").on("click", handleSubmit);
+  
+
 
 // function updateGauge(newdata){
   
@@ -406,7 +652,7 @@ function updateBar2(newdata) {
 
 
 
-// }
+// // }
 
 
 
--- Python_Conducttr/dropdown_work/app3.js
+++ Python_Conducttr/dropdown_work/app3.js
@@ -1,7 +1,4 @@
 
-var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-
-
 
 function autocomplete(inp, arr) {
     /*the autocomplete function takes two arguments,
@@ -100,18 +97,49 @@ function autocomplete(inp, arr) {
   });
 }
 
-autocomplete(document.getElementById("myInput"), countries);
+
+d3.json("player_data.json").then(function(data) {
+  var time_data=data[3].time_list;
+
+  autocomplete(document.getElementById("myInput"), time_data);
+  autocomplete(document.getElementById("myInput2"), time_data);
+  
+
+
+
+
+  
+  console.log(time_data)
+
+ 
+
+  // var inital_gauge_data=data[0].All;
+  // var initial_reply_data=data[1].All;        
+  // var initial_time_data=data[3].All;
+  
+  // updateBar(initial_reply_data);
+  // updateGauge(inital_gauge_data);
+  // updateLine(initial_time_data)
+  // updatePanel(initial_top_cont)
+}); 
+
+
+
 
 function handleSubmit() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
   
     // Select the input value from the form
-    var stock = d3.select("#myInput").node().value;
-    console.log(stock);
+    var start_date = d3.select("#myInput").node().value;
+    console.log(start_date);
+
+    var end_date = d3.select("#myInput2").node().value;
+    console.log(end_date);
   
     // clear the input value
     d3.select("#myInput").node().value = "";
+    d3.select("#myInput2").node().value = "";
   
     // Build the plot with the new stock
     // buildPlot(stock);
